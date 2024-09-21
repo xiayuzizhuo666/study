d1480270d43735c8db58f13f6ab3041cbab4ef6a
xiayuzizhuo666 <2964296252@qq.com>
2024-09-21 20:19:03 +0800

    Add files via upload

---
+++ test_7_29/test.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main()
+{
+	printf("hello world\n");
+	printf("hello world\n"); 
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	return 0;
+
+}//成功运行烧香拜佛
+//
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//
+//
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//               佛祖保佑         永无BUG
