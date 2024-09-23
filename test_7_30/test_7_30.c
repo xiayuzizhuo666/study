1caaca74bd4f4ad6004f33a412bd01b82cf47b4b
xiayuzizhuo666 <2964296252@qq.com>
2024-09-23 19:01:44 +0800

    start study 1 day

---
+++ test_7_30/test_7_30.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+//int main()
+//{
+	//printf(" % zu \n", sizeof(char));
+	//printf(" % zu \n", sizeof(short));
+	//printf(" % zu \n", sizeof(long));
+	//printf(" % zu \n", sizeof(long long));
+	//printf(" % zu \n", sizeof(int));
+	//printf(" % zu \n", sizeof(float));
+	//printf(" % zu \n", sizeof(double));
+//}
+//
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//
+//
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//               ·ð×æ±£ÓÓ         ÓÀÎÞBUG
+//int main()
+//{
+//	int age = 20;
+//	double price = 66.6;
+//
+//	return 0;
+//}
+//int main()
+//int b = 20;
+//{
+//	short age  =  20;
+//	int high  =  100;
+//	float weight = 88.5;
+//	int a = 10;
+//	return 0;
+//}
+int a = 100;
+
+int main()
+{
+	int a = 10;
+	printf("a=%d\n", a);
+
+	return 0;
+
+}
---
+++ test_7_30/test_7_30_1/test_7_30_1.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include  <stdio.h>
+//
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d %d", &num1,& num2);
+//	int sum = num1 + num2;
+//	printf("%zu\n", sum);
+//
+//	return 0;
+//
+//
+//}
+int a = 10;
+int  main()
+
+{
+		{
+
+		printf("a=%d\n", a);
+	}
+	printf("a=%d\n", a);
+	return 0;
+	}
