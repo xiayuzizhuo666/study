9ba62319b4f184485b41367ae4c649b1942336cc
瑕玉自琢 <2964296252@qq.com>
2024-09-27 16:18:04 +0800

    study day2

---
+++ test_8_7/test_8_7/add.c
@@ -0,0 +1,4 @@
+#define _crt_secure_no_warnings\
+
+
+int g_val = 2024;
---
+++ test_8_7/test_8_7/test_8_7.c
@@ -0,0 +1,52 @@
+#define _crt_secure_no_warnings\
+//typedef struct Node
+//{
+//	int data;
+//	struct Node* next
+//
+//}Node;
+//int main()
+//{
+//	typedef unsigned int uint;
+//	unsigned int num1 = 0;
+//	uint num2 = 1;
+//	struct Node n1;
+//	Node n2;
+//	return 0;
+//}
+#include <stdio.h>
+//void test()
+//{
+//	static int a = 1;
+//	a++;
+//	printf("%d", a);
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		test();
+//		i++;
+//	}
+//	
+//	return 0;
+//}
+//extern int g_val;
+//int main()
+//{
+//	printf("%d\n", g_val);
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 6;
+//	for (int i = 0; i < 3; i++)
+//	{
+//		printf("%d", a);
+//	}
+//
+//	return 0;
+//
+//}
