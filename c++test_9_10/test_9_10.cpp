ab1558a263cba04e8fe0d68eefaee360be374ea9
瑕玉自琢 <2964296252@qq.com>
2024-10-09 10:18:29 +0800

    study

---
+++ c++test_9_10/test_9_10.cpp
@@ -0,0 +1,11 @@
+//#include <iostream>
+//int main()
+//{
+//	std::cout << "enter two numbers:" << std::end1;
+//	int n1 = 0, n2 = 0;
+//	std::cin >> n1 >> n2;
+//	std::cout << "the sum of" << n1 << "and" << n2
+//		<< "is" << n1 + n2 << std::end1;
+//
+//	return 0;
+//}
