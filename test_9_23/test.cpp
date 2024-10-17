6d934ae4c5310c3a769a17d271a9fe9fe384e9f6
瑕玉自琢 <2964296252@qq.com>
2024-10-17 09:25:04 +0800

    study

---
+++ test_9_23/test.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	int i = 42;
+//
+//	if (i)
+//		i = 0;
+//
+//	return 0;
+//}
+//int main()
+//{
+//	unsigned u = 10;
+//	int i = -42;
+//
+//	cout << i + i << endl;
+//
+//	cout << u + i << endl;
+//
+//	unsigned u1 = 42, u2 = 10;
+//	cout << u1 - u2 << endl;
+//
+//	cout << u2 - u1 << endl;
+//	return 0;
+//}
+//int main()
+//{
+//	for (int i = 10; i >= 0; --i)
+//		cout << i << endl;
+//	return 0;
+//}
+//int main()
+//{
+//	for (unsigned u = 10; u >=0 ; --u)
+//	{
+//		cout << u << endl;
+//	}
+//
+//	return 0;
+//}
+//int main()
+//{
+//	unsigned u = 11;
+//
+//	while (u > 0)
+//	{
+//		--u;
+//		cout << u << endl;
+//	}
+//
+//	return 0;
+//}
+int main()
+{
+	unsigned u = 11;
+	while (u > 0)
+	{
+
+		--u;
+		cout << u << endl;
+	}
+	return 0;
+}
