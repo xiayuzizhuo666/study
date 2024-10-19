fc9ca686b5568ed643f49f0d58fe334577a2addb
瑕玉自琢 <2964296252@qq.com>
2024-10-20 00:32:59 +0800

    study

---
+++ test_9_29/test_9_28.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+//int main()
+//{
+//
+//	int i, & ri = i;
+//	i - 5, ri = 10;
+//
+//	cout << i << "" << ri << endl;
+//
+//
+//	return 0;
+//}
+int main()
+{
+	
+
+
+
+	system("puase");
+	return 0;
+}
