1452579399e7af04114962a3ccd7eec1be398173
瑕玉自琢 <2964296252@qq.com>
2024-10-21 23:57:01 +0800

    study

---
+++ test_10_6/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+typedef double wages;
+typedef wages base, * p;
+typedef char* pstring;
+
+int main()
+{
+	const pstring cstr = 0;
+	const pstring* ps;
+	
+
+
+	system("pause");
+	return 0;
+}
