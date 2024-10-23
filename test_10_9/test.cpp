10e0b6eb2d83024c59db739e3e5a026b5bf233e3
瑕玉自琢 <2964296252@qq.com>
2024-10-23 20:03:18 +0800

    study

---
+++ test_10_9/test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	vector<int> ivec;
+	vector<vector<string>>file;
+
+	vector <string> v5{ "hi" };
+	vector <string> v7{ 10 };
+	vector <string> v8{ 10,"hi" };
+
+
+
+	system("pause");
+	return 0;
+}
