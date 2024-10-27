a340d2b57e671acb297482c9e095f6d3f49d5490
瑕玉自琢 <2964296252@qq.com>
2024-10-27 23:14:26 +0800

    study

---
+++ test_10_13/test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+
+    //string ins;
+    //getline(cin, ins);
+    //cout << ins << endl;
+
+    system("pause");
+    return 0;
+}
