da02f793f8889e658bdd5c65b7c0db6615fedce9
瑕玉自琢 <2964296252@qq.com>
2024-11-03 21:56:54 +0800

    study

---
+++ test_10_20/源.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+//int main()
+//{
+//	unsigned scores[11] = {};
+//	unsigned grade;
+//	while (cin >> grade)
+//	{
+//		if (grade <= 100)
+//			++scores[grade / 10];
+//	}
+//	for (auto i:scores)
+//		cout << i << " ";
+//	cout << endl;
+//	return 0;
+//}
+
+
