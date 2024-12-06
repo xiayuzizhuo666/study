4c57f4422af12f3fc935f20f66454127165d6c5d
xiayuzizhuo <2964296252@qq.com>
2024-12-07 00:55:37 +0800

    study

---
+++ test_11_27/test.cpp
@@ -0,0 +1,54 @@
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//int main()
+//{
+//    int a0 = pow(7, 0);
+//    int a1 = pow(7, 1);
+//    int a2 = pow(7, 2);
+//    int a3 = pow(7, 3);
+//    int a4 = pow(7, 4);
+//    int a5 = pow(7, 5);
+//    int a6 = pow(7, 6);
+//    int a7 = pow(7, 7);
+//    for (int i = 0; i <= 5; i++)
+//    {
+//        for (int ii = 0; ii <= 5; ii++)
+//        {
+//            for (int iii = 0; iii <= 5; iii++)
+//            {
+//                for (int iiii = 0; iiii <= 5; iiii++)
+//                {
+//                    for (int iiiii = 0; iiiii <= 5; iiiii++)
+//                    {
+//                        for (int iiiiii = 0; iiiiii <= 5; iiiiii++)
+//                        {
+//                            for (int iiiiiii = 0; iiiiiii <= 5; iiiiiii++)
+//                            {
+//                                for (int iiiiiiii = 0; iiiiiiii <= 5; iiiiiiii++)
+//                                {
+//                                    int sum = a0 * i + a1 * ii + a2 * iii + a3 * iiii + a4 * iiiii + a5 * iiiiii + a6 * iiiiiii + a7 * iiiiiiii;
+//                                    if (sum == 1000000)
+//                                    {
+//                                        int count = i + ii + iii + iiii + iiiii + iiiiii + iiiiiii + iiiiiiii;
+//                                        cout << count << endl;
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//    return 0;
+//}
+
+//
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	printf("%d", 'L');
+//	return 0;
+//}
